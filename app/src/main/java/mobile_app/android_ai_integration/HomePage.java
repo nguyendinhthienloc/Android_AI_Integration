87214a0e791c3235d6edbb9ee58d5cdafb7d2729
Nguyen Trong Van Viet <nguyentrongvanviet@gmail.com>
2026-07-19 14:05:34 +0700

    Init project for XML + Java
    Currently there is only one blank screen with hello world in the home_page_activity.

---
+++ app/src/main/java/mobile_app/android_ai_integration/HomePage.java
@@ -0,0 +1,13 @@
+package mobile_app.android_ai_integration;
+import android.os.Bundle;
+
+import androidx.activity.ComponentActivity;
+
+public class HomePage extends ComponentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page_activity);
+    }
+}
